2ccbada5fba603469260b790364aa0a37ac2d3a2
ColdKing17 <42580887+ColdKing17@users.noreply.github.com>
2019-03-03 14:37:01 +0200

    Add files via upload

--- FirstSite/mainapp/urls.py
+++ FirstSite/mainapp/urls.py
@@ -2,5 +2,30 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path("new1/", views.new1, name="new1"),
+    path("new2/", views.new2, name="new2"),
+    path("new3/", views.new3, name="new3"),
+    path("new4/", views.new4, name="new4"),
+    path("new5/", views.new5, name="new5"),
+    path("new6/", views.new6, name="new6"),
+    path("new7/", views.new7, name="new7"),
+    path("new8/", views.new8, name="new8"),
+    path("new9/", views.new9, name="new9"),
+    path("new10/", views.new10, name="new10"),
+    path("new11/", views.new11, name="new11"),
+    path("new12/", views.new12, name="new12"),
+    path("new13/", views.new13, name="new13"),
+    path("new14/", views.new14, name="new14"),
+    path("new15/", views.new15, name="new15"),
+    path("new16/", views.new16, name="new16"),
+    path("new17/", views.new17, name="new17"),
+    path("new18/", views.new18, name="new18"),
+    path("new19/", views.new19, name="new19"),
+    path("new20/", views.new20, name="new20"),
+    path("new21/", views.new21, name="new21"),
+    path("new22/", views.new22, name="new22"),
+    path("new23/", views.new23, name="new23"),
+    path("new24/", views.new24, name="new24"),
+    path("new25/", views.new25, name="new25")
 ]
--- FirstSite/mainapp/views.py
+++ FirstSite/mainapp/views.py
@@ -2,4 +2,79 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "wrapper.html")
+    return render(request, "main.html")
+
+def new1(request):
+    return render(request, "new 1.html")
+
+def new2(request):
+    return render(request, "new 2.html")
+
+def new3(request):
+    return render(request, "new 3.html")
+
+def new4(request):
+    return render(request, "new 4.html")
+
+def new5(request):
+    return render(request, "new 5.html")
+
+def new6(request):
+    return render(request, "new 6.html")
+
+def new7(request):
+    return render(request, "new 7.html")
+
+def new8(request):
+    return render(request, "new 8.html")
+
+def new9(request):
+    return render(request, "new 9.html")
+
+def new10(request):
+    return render(request, "new 10.html")
+
+def new11(request):
+    return render(request, "new 11.html")
+
+def new12(request):
+    return render(request, "new 12.html")
+
+def new13(request):
+    return render(request, "new 13.html")
+
+def new14(request):
+    return render(request, "new 14.html")
+
+def new15(request):
+    return render(request, "new 15.html")
+
+def new16(request):
+    return render(request, "new 16.html")
+
+def new17(request):
+    return render(request, "new 17.html")
+
+def new18(request):
+    return render(request, "new 18.html")
+
+def new19(request):
+    return render(request, "new 19.html")
+
+def new20(request):
+    return render(request, "new 20.html")
+
+def new21(request):
+    return render(request, "new 21.html")
+
+def new22(request):
+    return render(request, "new 22.html")
+
+def new23(request):
+    return render(request, "new 23.html")
+
+def new24(request):
+    return render(request, "new 24.html")
+
+def new25(request):
+    return render(request, "new 25.html")
